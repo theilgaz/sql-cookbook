4c6f5ec0e381d28072c8f1f4155d8de9f0a6b0fa
Abdullah ILGAZ <mail@abdullahilgaz.com>
2020-04-29 12:08:15 +0300

    Update Get_Table_and_View_List_With_SchemaName.sql

--- src/List/Get_Table_and_View_List_With_SchemaName.sql
+++ src/List/Get_Table_and_View_List_With_SchemaName.sql
@@ -1,2 +1,3 @@
+-- Author: Abdullah ILGAZ, abdullah@entegreyazilim.com.tr
 -- object_id 949578421 --> sysdiagrams
 SELECT name, SCHEMA_NAME(schema_id) as schemaname FROM sys.objects WHERE object_id <> '949578421' and type in ('U','V') order by name, type
