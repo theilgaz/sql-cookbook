874510a25e5f497133218254f6d7d4734a6757fa
Abdullah ILGAZ <mail@abdullahilgaz.com>
2020-04-29 12:23:10 +0300

    Update Get_Table_and_View_List_With_SchemaName.sql

--- src/List/Get_Table_and_View_List_With_SchemaName.sql
+++ src/List/Get_Table_and_View_List_With_SchemaName.sql
@@ -2,4 +2,5 @@
 -- Output: Gives you a list of tables/views and own schema names.
 -- Sample: TableName, SchemaName
 -- Notes: object_id 949578421 --> sysdiagrams
+
 SELECT name, SCHEMA_NAME(schema_id) as schemaname FROM sys.objects WHERE object_id <> '949578421' and type in ('U','V') order by name, type
