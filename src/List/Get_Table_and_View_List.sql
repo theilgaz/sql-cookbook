b10a0877e78704d0afe2ab4ddaa46e1013d3337d
Abdullah ILGAZ <mail@abdullahilgaz.com>
2020-04-29 12:08:13 +0300

    Update Get_Table_and_View_List.sql

--- src/List/Get_Table_and_View_List.sql
+++ src/List/Get_Table_and_View_List.sql
@@ -1,2 +1,3 @@
+-- Author: Abdullah ILGAZ, abdullah@entegreyazilim.com.tr
 -- object_id 949578421 --> sysdiagrams
 SELECT name FROM sys.objects WHERE object_id <> '949578421' and type in ('U','V') order by name, type
