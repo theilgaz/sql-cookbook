d7b4d8c6662b82ed2ba770da8f7c25d3dc2edfc7
Abdullah ILGAZ <mail@abdullahilgaz.com>
2020-04-29 12:22:43 +0300

    Update Get_Current_SQL_Version.sql

--- src/Basics/Get_Current_SQL_Version.sql
+++ src/Basics/Get_Current_SQL_Version.sql
@@ -1,2 +1,5 @@
 -- Author: Abdullah ILGAZ, abdullah@entegreyazilim.com.tr
+-- Output: Gives you a quick summary for current sql server and operating system with version and architecture informations.
+-- Sample: Microsoft SQL Server 2017 (RTM-GDR) (KB4505224) - 14.0.2027.2 (X64) etc...
+
 select @@VERSION as V
