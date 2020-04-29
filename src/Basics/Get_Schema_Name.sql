18d4bf9c93615d2f5fcc32a62921d690cb3a0305
Abdullah ILGAZ <mail@abdullahilgaz.com>
2020-04-29 12:20:47 +0300

    Update Get_Schema_Name.sql

--- src/Basics/Get_Schema_Name.sql
+++ src/Basics/Get_Schema_Name.sql
@@ -1,2 +1,5 @@
 -- Author: Abdullah ILGAZ, abdullah@entegreyazilim.com.tr
+-- Output: Gives you current schema name for desired table.
+-- Sample: SchemaName
+
 SELECT SCHEMA_NAME(schema_id) AS SchemaName FROM sys.tables where name='TABLE_NAME';
