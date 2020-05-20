05f5a66b4148cd80db8aff46711aa616b54b343f
Şeref ILGAZ <serefilgaz@gmail.com>
2020-05-20 18:14:12 +0300

    Create Kill_Active_Transactions.sql

---
+++ src/Advanced/Kill_Active_Transactions.sql
@@ -0,0 +1,7 @@
+-- Author: Şeref ILGAZ seref@entegreyazilim.com.tr, Abdullah ILGAZ, abdullah@entegreyazilim.com.tr
+-- Output: Gives you active sessions and transactions
+
+
+execute sp_who
+execute sp_who2 
+execute sp_xtp_kill_active_transactions [DBNAME]
