47822f981c4138c21cbc4b45cf241ea72aa865f4
Abdullah Ilgaz <mail@abdullahilgaz.com>
2020-05-27 18:11:26 +0300

    Update Get_Table_and_View_List.sql

--- src/List/Get_Table_and_View_List.sql
+++ src/List/Get_Table_and_View_List.sql
@@ -1,6 +1,6 @@
 -- Author: Abdullah ILGAZ, abdullah@entegreyazilim.com.tr
 -- Output: Gives you a list of tables/views.
 -- Sample: TableName
--- Notes: object_id 949578421 --> sysdiagrams
+-- Notes: without sysdiagrams
 
-SELECT name FROM sys.objects WHERE object_id <> '949578421' and type in ('U','V') order by name, type
+SELECT name FROM sys.objects WHERE name <> 'sysdiagrams' and type in ('U','V') order by name, type
