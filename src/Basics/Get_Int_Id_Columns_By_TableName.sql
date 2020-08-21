3e974caca949a9273cbfc692167b6cac81b1277a
Abdullah Ilgaz <mail@abdullahilgaz.com>
2020-08-21 09:54:52 +0300

    Update Get_Int_Id_Columns_By_TableName.sql

--- src/Basics/Get_Int_Id_Columns_By_TableName.sql
+++ src/Basics/Get_Int_Id_Columns_By_TableName.sql
@@ -2,4 +2,4 @@
 -- Output: 
 -- Sample: int XId
 
-select ColumnType, ColumnName from ( select replace(col.name, ' ', '_') ColumnName, column_id ColumnId, case typ.name when 'int' then 'int' end ColumnType from sys.columns col join sys.types typ on col.system_type_id = typ.system_type_id AND col.user_type_id = typ.user_type_id where object_id = object_id('APP.City') and typ.name='int' and col.name like '%Id' and col.name <> 'Id') t order by ColumnId
+select ColumnType, ColumnName from ( select replace(col.name, ' ', '_') ColumnName, column_id ColumnId, case typ.name when 'int' then 'int' end ColumnType from sys.columns col join sys.types typ on col.system_type_id = typ.system_type_id AND col.user_type_id = typ.user_type_id where object_id = object_id('SCHEMA_NAME.TABLE_NAME') and typ.name='int' and col.name like '%Id' and col.name <> 'Id') t order by ColumnId
